acdbd5a3866188f4add7c48e9f43551620422f5a
AndyyyPhan <80480649+AndyyyPhan@users.noreply.github.com>
2025-04-21 20:39:25 -0400

    Added highlight box and connected it to home.js

--- views/home.php
+++ views/home.php
@@ -5,13 +5,35 @@ Kevin Arleen's Contributions: workout-plan.html, workout-tracker.html
 -->
 
 <?php include(__DIR__ . '/header.php'); ?>
+<style>
+    #muscle-container {
+    position: relative;
+    width: 500px;
+    margin: auto;
+}
+
+    .highlight-box {
+        position: absolute;
+        background-color: rgba(0,255,0,0.3);
+        border: 2px solid green;
+        display: none;
+        pointer-events: none;
+        z-index: 10; /* must be higher than image */
+    }
+</style>
 <header class="container text-center my-5">
     <h1>Welcome to MuscleMap</h1>
     <p class="lead">Explore muscle groups and discover the best exercises to strengthen them.</p>
 </header>
 <div class="container text-center my-4">
-    <a href="index.php?command=exercise">
-        <img src="assets/human_body.png" alt="Human Body Diagram" class="img-fluid">
-    </a>
-</div>        
+  <div id="muscle-container">
+    <img id="human-body" src="assets/human_body.png" alt="Human Body Diagram" width="500" usemap="#musclemap">
+    <div id="highlight" class="highlight-box"></div>
+    <map name="musclemap">
+        <area shape="rect" coords="130,150,180,200" href="index.php?command=exercise&muscle=biceps" alt="Biceps" id="biceps">
+        <area shape="rect" coords="200,300,250,350" href="index.php?command=exercise&muscle=quads" alt="Quads" id="quads">
+  </map>
+  </div>
+</div>
+<script src="/tmq6ed/musclemap/assets/js/home.js"></script>       
 <?php include(__DIR__ . '/footer.php');?>
