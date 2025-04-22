48819630eca6c4051379fff5ca2620b66d5501a5
KAJRX <KevinJArleen@gmail.com>
2025-04-21 23:25:34 -0400

    Aligned muscle highlightingand new muscle groups

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const highlight = document.getElementById("highlight");
   
     const muscleMap = {
-      biceps: { x: 130, y: 150, w: 50, h: 50 },
-      quads: { x: 200, y: 300, w: 50, h: 50 }
+      biceps: { x: 170, y: 130, w: 50, h: 50 },
+      quads: { x: 130, y: 270, w: 50, h: 50 },
+      triceps: { x: 270, y: 130, w: 50, h: 50},
+      hamstrings: { x: 300, y: 270, w: 50, h: 50},
+      calves: { x: 300, y: 350, w: 50, h: 50},
+      back: { x: 330, y: 110, w: 50, h: 50},
+      chest: { x: 100,y: 90, w: 50, h: 50}
     };
   
     function showHighlight(muscleId) {
@@ -26,4 +31,19 @@ document.addEventListener("DOMContentLoaded", () => {
   
     document.querySelector("area#quads").addEventListener("mouseenter", () => showHighlight("quads"));
     document.querySelector("area#quads").addEventListener("mouseleave", hideHighlight);
+
+    document.querySelector("area#triceps").addEventListener("mouseenter", () => showHighlight("triceps"));
+    document.querySelector("area#triceps").addEventListener("mouseleave", hideHighlight);
+
+    document.querySelector("area#hamstrings").addEventListener("mouseenter", () => showHighlight("hamstrings"));
+    document.querySelector("area#hamstrings").addEventListener("mouseleave", hideHighlight);
+
+    document.querySelector("area#calves").addEventListener("mouseenter", () => showHighlight("calves"));
+    document.querySelector("area#calves").addEventListener("mouseleave", hideHighlight);
+
+    document.querySelector("area#back").addEventListener("mouseenter", () => showHighlight("back"));
+    document.querySelector("area#back").addEventListener("mouseleave", hideHighlight);
+
+    document.querySelector("area#chest").addEventListener("mouseenter", () => showHighlight("chest"));
+    document.querySelector("area#chest").addEventListener("mouseleave", hideHighlight);
   });  
