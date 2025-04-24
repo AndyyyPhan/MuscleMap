c33a8f6e99190b3cefdf7c5aec2cd40458417e8b
AndyyyPhan <80480649+AndyyyPhan@users.noreply.github.com>
2025-04-24 10:23:29 -0400

    Updated Quadriceps to Quads to work with highlighting muscle group in human_body on home.php

--- view-database.php
+++ view-database.php
@@ -13,13 +13,24 @@ if ($db) {
     die("Failed to connect to database");
 }
 
-$result = pg_query($db, "
-    SELECT tablename 
-    FROM pg_catalog.pg_tables 
-    WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';
-");
+// $result = pg_query($db, "
+//     SELECT tablename 
+//     FROM pg_catalog.pg_tables 
+//     WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';
+// ");
+
+$result = pg_query($db, "SELECT DISTINCT muscle_group FROM musclemap_exercises;");
 
 $rows = pg_fetch_all($result);
+
+// $query = "UPDATE musclemap_exercises SET muscle_group = 'Quads' WHERE muscle_group = 'Quadriceps'";
+// $result = pg_query($db, $query);
+
+// if ($result) {
+//     echo "Muscle group updated from 'Quadriceps' to 'quads'.";
+// } else {
+//     echo "Error: " . pg_last_error($db);
+// }
 
 echo "<pre>";
 print_r($rows);
